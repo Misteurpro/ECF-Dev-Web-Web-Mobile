61706e33fe74ba54410cc4f0fef217aed4d3ddac
Misteurpro <fredericchippaux@gmail.com>
2026-09-21 14:58:56 +0200

    added CSRF_Token to Approve-comment.php and modification of get_comments_to_approve()

--- lib/function.php
+++ lib/function.php
@@ -740,8 +740,8 @@ function get_comments_to_approve(int $page, int $limit) : mixed{
 						<div class="date-approval">
 							<p class="comment-date"><img width="20" height="20" src="/assets/frontend/SVG/Icones/calendar-lines-pen.svg" alt=""><?php echo $comment['date_commentaire'] ?></p>
 							<div class="buttons-div">
-								<a href='/<?php echo $url ?>/approuver-commentaire?commentaire=<?php echo $comment["id_commentaire"] ?>&approve' class='button color-secondary-darker'>Approuver</a>
-								<a href='/<?php echo $url ?>/approuver-commentaire?commentaire=<?php echo $comment["id_commentaire"] ?>&refuse' class='button color-red'>Refuser</a>
+								<a href='/<?php echo $url ?>/approuver-commentaire?commentaire=<?php echo $comment["id_commentaire"] ?>&csrf_token=<?php echo get_csrf_token() ?>&approve' class='button color-secondary-darker'>Approuver</a>
+								<a href='/<?php echo $url ?>/approuver-commentaire?commentaire=<?php echo $comment["id_commentaire"] ?>&csrf_token=<?php echo get_csrf_token() ?>&refuse' class='button color-red'>Refuser</a>
 							</div>
 						</div>
 					</div>
